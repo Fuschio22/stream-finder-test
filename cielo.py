95e78d56f5828feb17b5051a6c459e268ae4b536
Fuschio22 <30805651+Fuschio22@users.noreply.github.com>
2026-06-17 22:57:11 +0200

    Update cielo.py

--- cielo.py
+++ cielo.py
@@ -1,26 +1,15 @@
 from playwright.sync_api import sync_playwright
+import re
 
-interesting = []
-
-KEYWORDS = [
-    "token",
-    "auth",
-    "play",
-    "playback",
-    "stream",
-    "video",
-    "live",
-    "channel",
-    "manifest",
-    "session",
-    "api"
-]
+stream_url = None
 
 def handle_response(response):
-    url = response.url.lower()
+    global stream_url
 
-    if any(k in url for k in KEYWORDS):
-        interesting.append(f"{response.status} | {response.url}")
+    url = response.url
+
+    if ".m3u8" in url and "cieloweb" in url:
+        stream_url = url
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -37,11 +26,14 @@ with sync_playwright() as p:
         timeout=60000
     )
 
-    page.wait_for_timeout(20000)
+    page.wait_for_timeout(15000)
 
     browser.close()
 
-print("\n=== RICHIESTE INTERESSANTI ===\n")
+if stream_url:
+    print(f"TROVATO:\n{stream_url}")
 
-for item in sorted(set(interesting)):
-    print(item)
+    with open("cielo.txt", "w") as f:
+        f.write(stream_url)
+else:
+    print("Nessun stream trovato")
